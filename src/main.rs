8f40ad72e972e67ab91d7b82ccedbe5f700635f1
fddmamtnl <61471363+fddmamtnl@users.noreply.github.com>
2022-09-27 02:44:12 +0900

    ゲームフォルダの読み込み機能を追加

---
+++ src/main.rs
@@ -0,0 +1,53 @@
+use bevy::prelude::*;
+use serde::Deserialize;
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
+
+struct Game {
+    path: PathBuf,
+    title: String,
+    description: String,
+    author: String,
+    screenshot: Handle<Image>,
+}
+
+#[allow(dead_code)]
+#[derive(Deserialize, Debug)]
+struct GameSummary {
+    title: String,
+    description: String,
+    author: String,
+}
+
+struct Games(Vec<Game>);
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .insert_resource(Games(Vec::new()))
+        .add_startup_system(load_game_folder)
+        .run();
+}
+
+fn load_game_folder(mut games: ResMut<Games>, asset_server: Res<AssetServer>) {
+    for d in fs::read_dir("games").unwrap().filter_map(|e| e.ok()) {
+        info!("{:?}", d.path());
+        let game_summary = get_game_summary(d.path());
+        info!("{:?}", game_summary);
+        games.0.push(Game {
+            path: d.path().join("game.exe"),
+            title: game_summary.title,
+            description: game_summary.description,
+            author: game_summary.author,
+            screenshot: asset_server.load(d.path().join("screenshot.png")),
+        })
+    }
+}
+
+fn get_game_summary<P: AsRef<Path>>(path: P) -> GameSummary {
+    let summary_file = path.as_ref().join("launcher_summary.toml");
+    let summary_file_content = fs::read_to_string(summary_file).unwrap();
+    toml::from_str::<GameSummary>(&summary_file_content).unwrap()
+}
